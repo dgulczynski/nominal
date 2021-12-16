e1682172883d068ceb29c6699712cc943c0c4033
Domin <dominik.gulczynski@gmail.com>
2021-12-16 20:19:19 +0100

    Further refactor solver

--- src/Solver.ml
+++ src/Solver.ml
@@ -83,39 +83,48 @@ module Solver = struct
     | Fun f, Fun f' -> f = f'
     | _ -> false
 
-  and solve_ env assmpts c =
+  and solve_ env assmpts goal =
     match reduce env assmpts with
-    | None, _, _                  -> true
-    | Some env', assmpts', assmpt ->
-    match assmpt with
-    | Some (Fresh (a, Atom {perm= pi; symb= b})) | Some (AtomNeq (a, {perm= pi; symb= b}))
-      ->
+    | None, _, _ -> true
+    | Some env', assmpts', None -> solve_by_case env' assmpts' goal
+    | Some env', assmpts', Some assmpt -> solve_assumption env' assmpts' goal assmpt
+
+  and solve_assumption env assmpts goal = function
+    | Fresh (a, Atom {perm= pi; symb= b}) | AtomNeq (a, {perm= pi; symb= b}) ->
         let (alpha1, alpha2), pi' = Option.get $ outer_swap pi in
         (* TODO: convert to cps *)
-        solve_ env'
+        solve_ env
           ( AtomNeq (a, alpha1)
           :: AtomNeq (a, alpha2)
           :: AtomNeq (a, {perm= pi'; symb= b})
-          :: assmpts' )
-          c
-        && solve_ env'
+          :: assmpts )
+          goal
+        && solve_ env
              ( AtomEq (b, alpha1)
              :: AtomNeq (b, alpha2)
              :: AtomNeq (a, permute pi' alpha2)
-             :: assmpts' )
-             c
-        && solve_ env'
-             (AtomEq (b, alpha2) :: AtomNeq (a, permute pi' alpha1) :: assmpts')
-             c
-    | Some (AtomEq (a, {perm= []; symb= b})) ->
-        let senv = SolverEnv.subst_atom env' a b in
-        let sassmpts = List.map (subst_atom_constr a b) assmpts' in
-        let sc = subst_atom_constr a b c in
-        solve_ senv sassmpts sc
-    | _ ->
-    match c with
-    | Eq (t1, t2)  -> solve_eq env' assmpts' t1 t2
-    | Fresh (a, t) -> solve_fresh env' assmpts' a t
+             :: assmpts )
+             goal
+        && solve_ env
+             (AtomEq (b, alpha2) :: AtomNeq (a, permute pi' alpha1) :: assmpts)
+             goal
+    | AtomEq (a, {perm= []; symb= b}) ->
+        let env' = SolverEnv.subst_atom env a b in
+        let assmpts' = List.map (subst_atom_constr a b) assmpts in
+        let goal' = subst_atom_constr a b goal in
+        solve_ env' assmpts' goal'
+    | assmpt ->
+        let error_str =
+          "This should not happen: Assumption "
+          ^ Printing.string_of_constr assmpt
+          ^ " was chosen to be reduced, but solver doesn't know how to reduce it"
+        in
+        let _ = Printf.eprintf "%s\n" error_str in
+        solve_by_case env (assmpt :: assmpts) goal
+
+  and solve_by_case env assmpts = function
+    | Eq (t1, t2)  -> solve_eq env assmpts t1 t2
+    | Fresh (a, t) -> solve_fresh env assmpts a t
     | _            -> false
 
   let solve_with_env env = solve_ env []
