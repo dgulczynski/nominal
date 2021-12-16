44f849ce36f14bb63ae21cb676eb5b415f12ab95
Domin <dominik.gulczynski@gmail.com>
2021-12-16 11:38:35 +0100

    Refactor solver

--- src/Solver.ml
+++ src/Solver.ml
@@ -27,50 +27,53 @@ module Solver = struct
 
   let rec solve_fresh env assmpts a e =
     match e with
-    | Atom {perm= []; symb= b} -> SolverEnv.is_neq env a b
-    | Atom {perm= (alpha1, alpha2) :: pi; symb= b} ->
-        (* TODO: convert to cps *)
-        solve_ env (AtomNeq (b, alpha1) :: AtomNeq (b, alpha2) :: assmpts)
-        $ Fresh (a, Atom {perm= pi; symb= b})
-        && solve_ env (AtomEq (b, alpha1) :: assmpts) $ Fresh (a, Atom (permute pi alpha2))
-        && solve_ env (AtomEq (b, alpha2) :: assmpts) $ Fresh (a, Atom (permute pi alpha1))
-    | Var {perm= []; symb= x} -> SolverEnv.is_fresh env a x
-    | Var {perm= (_alpha1, _alpha2) :: _pi; symb= _x} ->
-        (* TODO: implement *)
-        false
-    | Lam ({perm= []; symb= b}, _) when a = b -> true
-    | Lam (alpha, t) ->
-        let assmpts' = AtomNeq (a, alpha) :: assmpts in
-        solve_ env assmpts' $ Fresh (a, t)
-    | App (t1, t2) -> solve_fresh env assmpts a t1 && solve_fresh env assmpts a t2
-    | Fun _ -> true
+    | Atom {perm; symb= b} -> (
+      match inner_swap perm with
+      | None                        -> SolverEnv.is_neq env a b
+      | Some ((alpha1, alpha2), pi) ->
+          (* TODO: convert to cps *)
+          solve_ env (AtomNeq (b, alpha1) :: AtomNeq (b, alpha2) :: assmpts)
+          $ Fresh (a, Atom {perm= pi; symb= b})
+          && solve_ env (AtomEq (b, alpha1) :: assmpts)
+             $ Fresh (a, Atom (permute pi alpha2))
+          && solve_ env (AtomEq (b, alpha2) :: assmpts)
+             $ Fresh (a, Atom (permute pi alpha1)) )
+    | Var {perm; symb= x}  -> (
+      match outer_swap perm with
+      | None                        -> SolverEnv.is_fresh env a x
+      | Some ((alpha1, alpha2), pi) ->
+          let c = Fresh (a, Var {perm= pi; symb= x}) in
+          (* TODO: convert to cps *)
+          solve_ env (AtomNeq (a, alpha1) :: AtomNeq (a, alpha2) :: assmpts) c
+          && solve_ env (AtomNeq (a, alpha1) :: AtomEq (a, alpha2) :: assmpts) c
+          && solve_ env (AtomEq (a, alpha1) :: assmpts) c )
+    | Lam (alpha, t)       -> solve_ env (AtomNeq (a, alpha) :: assmpts) $ Fresh (a, t)
+    | App (t1, t2)         -> solve_fresh env assmpts a t1 && solve_fresh env assmpts a t2
+    | Fun _                -> true
 
   and solve_eq env assmpts e1 e2 =
-    (* TODO: improve readability *)
     match (e1, e2) with
-    | Atom {perm= []; symb= a}, Atom {perm= pi; symb= b} -> (
-      match outer_swap pi with
-      | None                         -> a = b
-      | Some ((alpha1, alpha2), pi') ->
+    | Atom {perm= []; symb= a}, Atom {perm; symb= b} -> (
+      match outer_swap perm with
+      | None                        -> a = b
+      | Some ((alpha1, alpha2), pi) ->
+          let b' = Atom {perm= pi; symb= b} in
           (* TODO: convert to cps *)
-          solve_ env (AtomNeq (a, alpha1) :: AtomNeq (a, alpha2) :: assmpts)
-          $ Eq (e1, Atom {perm= pi'; symb= b})
+          solve_ env (AtomNeq (a, alpha1) :: AtomNeq (a, alpha2) :: assmpts) $ Eq (e1, b')
           && solve_ env (AtomNeq (a, alpha1) :: AtomEq (a, alpha2) :: assmpts)
-             $ Eq (Atom alpha1, Atom {perm= pi'; symb= b})
-          && solve_ env (AtomEq (a, alpha1) :: assmpts)
-             $ Eq (Atom alpha2, Atom {perm= pi'; symb= b}) )
+             $ Eq (Atom alpha1, b')
+          && solve_ env (AtomEq (a, alpha1) :: assmpts) $ Eq (Atom alpha2, b') )
     | Atom {perm= pi; symb= a}, Atom b ->
-        solve_eq env assmpts $ Atom {perm= []; symb= a} $ Atom (permute pi b)
-    | Var {perm= []; symb= x}, Var {perm= []; symb= x'} -> x = x'
-    | Var {perm= []; symb= x}, Var {perm= pi; symb= x'} when x = x' ->
+        solve_eq env assmpts $ Atom (pure a) $ Atom (permute (reverse pi) b)
+    | Var {perm= []; symb= x}, Var {perm= pi; symb= x'} ->
         (* TODO: convert to cps *)
         let test ({perm= pi; symb= a} as alpha) =
           solve_eq env assmpts (Atom alpha) (Atom (permute pi alpha))
           || solve_fresh env assmpts a $ Var {perm= reverse pi; symb= x}
         in
-        List.for_all test $ free_vars_of pi
+        x = x' && List.for_all test $ free_vars_of pi
     | Var {perm= pi; symb= x}, Var x' ->
-        solve_eq env assmpts $ Var {perm= []; symb= x} $ Var (permute pi x')
+        solve_eq env assmpts $ Var (pure x) $ Var (permute (reverse pi) x')
     | Lam (({perm= pi; symb= a1} as alpha1), t1), Lam (alpha2, t2) ->
         (* TODO: convert to cps *)
         solve_fresh env assmpts a1 $ permute_term (reverse pi) e2
@@ -82,9 +85,11 @@ module Solver = struct
 
   and solve_ env assmpts c =
     match reduce env assmpts with
-    | None, _, _ -> true
-    | Some env', assmpts', Some (Fresh (a, Atom {perm= pi; symb= b}))
-     |Some env', assmpts', Some (AtomNeq (a, {perm= pi; symb= b})) ->
+    | None, _, _                  -> true
+    | Some env', assmpts', assmpt ->
+    match assmpt with
+    | Some (Fresh (a, Atom {perm= pi; symb= b})) | Some (AtomNeq (a, {perm= pi; symb= b}))
+      ->
         let (alpha1, alpha2), pi' = Option.get $ outer_swap pi in
         (* TODO: convert to cps *)
         solve_ env'
@@ -102,12 +107,12 @@ module Solver = struct
         && solve_ env'
              (AtomEq (b, alpha2) :: AtomNeq (a, permute pi' alpha1) :: assmpts')
              c
-    | Some env', assmpts', Some (AtomEq (a, {perm= []; symb= b})) ->
+    | Some (AtomEq (a, {perm= []; symb= b})) ->
         let senv = SolverEnv.subst_atom env' a b in
         let sassmpts = List.map (subst_atom_constr a b) assmpts' in
         let sc = subst_atom_constr a b c in
         solve_ senv sassmpts sc
-    | Some env', assmpts', _ ->
+    | _ ->
     match c with
     | Eq (t1, t2)  -> solve_eq env' assmpts' t1 t2
     | Fresh (a, t) -> solve_fresh env' assmpts' a t
