005336436e16638900670e2405119d21fae8d231
Domin <dominik.gulczynski@gmail.com>
2022-01-10 23:56:06 +0100

    Update printing

--- src/Printing.ml
+++ src/Printing.ml
@@ -1,18 +1,18 @@
 open Types
 open Permutation
 
+let string_of_atom_arg a = a
+
 let rec string_of_permutation pi =
   List.fold_left
     (fun str (a, b) -> "(" ^ string_of_atom a ^ " " ^ string_of_atom b ^ ")" ^ str)
     "" pi
 
-and string_of_atom = function
-  | {perm= []; symb= A a} -> a
-  | {perm= pi; symb= A a} -> string_of_permutation pi ^ a
+and string_of_atom {perm= pi; symb= A a} = string_of_permutation pi ^ string_of_atom_arg a
 
-let string_of_var = function
-  | {perm= []; symb= V x} -> x
-  | {perm= pi; symb= V x} -> string_of_permutation pi ^ x
+let string_of_var_arg v = v
+
+let string_of_var {perm= pi; symb= V x} = string_of_permutation pi ^ string_of_var_arg x
 
 let rec string_of_term = function
   | Atom a       -> string_of_atom a
@@ -22,9 +22,9 @@ let rec string_of_term = function
   | Fun f        -> f
 
 let string_of_constr = function
-  | Fresh (A a, t)       -> a ^ " #: " ^ string_of_term t
   | Eq (t1, t2)          -> string_of_term t1 ^ " =: " ^ string_of_term t2
-  | AtomEq (A a, alpha)  -> a ^ " ==: " ^ string_of_atom alpha
-  | AtomNeq (A a, alpha) -> a ^ " !=: " ^ string_of_atom alpha
+  | Fresh (A a, t)       -> string_of_atom_arg a ^ " #: " ^ string_of_term t
+  | AtomEq (A a, alpha)  -> string_of_atom_arg a ^ " ==: " ^ string_of_atom alpha
+  | AtomNeq (A a, alpha) -> string_of_atom_arg a ^ " =/=: " ^ string_of_atom alpha
   | Shape (t1, t2)       -> string_of_term t1 ^ " ~: " ^ string_of_term t2
   | Subshape (t1, t2)    -> string_of_term t1 ^ " <: " ^ string_of_term t2
