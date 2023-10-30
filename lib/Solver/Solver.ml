8078993b2760b27e6299624bda0387ec439929b2
dgulczynski <dominik.gulczynski@gmail.com>
2023-10-31 00:36:37 +0100

    Simplify solve_by_case procedure

--- lib/Solver/Solver.ml
+++ lib/Solver/Solver.ml
@@ -24,17 +24,17 @@ let reduce env =
 let rec solve env assms goal =
   match reduce env assms with
   | None, _ -> true
-  | Some env, [] -> solve_by_case env [] goal
+  | Some env, [] -> solve_by_case env goal
   | Some env, assm :: assms -> solve_assm_by_case env assms goal assm
 
-and solve_by_case env assms goal =
+and solve_by_case env goal =
   match goal with
-  | SC_Eq (t1, t2) -> solve_eq env assms t1 t2
-  | SC_AtomEq (a, alpha) -> solve_eq env assms (atom a) (atom' alpha)
-  | SC_Fresh (a, t) -> solve_fresh env assms a t
-  | SC_AtomNeq (a, alpha) -> solve_fresh env assms a (atom' alpha)
-  | SC_Shape (s1, s2) -> solve_shape env assms s1 s2
-  | SC_Subshape (s1, s2) -> solve_subshape env assms s1 s2
+  | SC_Eq (t1, t2) -> solve_eq env t1 t2
+  | SC_AtomEq (a, alpha) -> solve_eq env (atom a) (atom' alpha)
+  | SC_Fresh (a, t) -> solve_fresh env a t
+  | SC_AtomNeq (a, alpha) -> solve_fresh env a (atom' alpha)
+  | SC_Shape (s1, s2) -> solve_shape env s1 s2
+  | SC_Subshape (s1, s2) -> solve_subshape env s1 s2
   | SC_Symbol s -> solve_symbol env s
 
 and solve_assm_by_case env assms goal assm =
@@ -47,188 +47,187 @@ and solve_assm_by_case env assms goal assm =
   | SC_Subshape (s1, s2) -> solve_assm_subshape env assms goal s1 s2
   | SC_Symbol s -> solve_assm_symbol env assms goal s
 
-and solve_eq env assms e1 e2 =
+and solve_eq env e1 e2 : bool =
   match (e1, e2) with
   | T_Atom {perm= []; symb= a}, T_Atom {perm; symb= b} -> (
     match outer_swap perm with
     | None ->
-      (* --------------- *)
-      (*  Γ, Δ |- a = a  *)
+      (* ---------------- *)
+      (*  [], Δ |- a = a  *)
       a = b
     | Some (swap, pi) ->
-      (*  (a # a1) :: (a # a2) :: Γ, Δ |-  a = π b  *)
-      (*  (a = a1) :: (a # a2) :: Γ, Δ |- a2 = π b  *)
-      (*              (a = a2) :: Γ, Δ |- a1 = π b  *)
-      (* ------------------------------------------ *)
-      (*            Γ, Δ |- a = [a1 a2]π b          *)
+      (*  [ a # a1, a # a2 ], Δ |-  a = π b  *)
+      (*  [ a = a1, a # a2 ], Δ |- a2 = π b  *)
+      (*          [ a = a2 ], Δ |- a1 = π b  *)
+      (* ----------------------------------- *)
+      (*        [], Δ |- a = [a1 a2]π b      *)
       let gen_goal a = atom' a =: atom' {perm= pi; symb= b} in
-      solve_swap_cases env a swap (const assms) gen_goal )
+      solve_swap_cases env a swap (const []) gen_goal )
   | T_Atom {perm= pi; symb= a}, T_Atom b ->
-    (*  Γ, Δ |- a = (rev π @ π') b  *)
-    (* ---------------------------- *)
-    (*     Γ, Δ |- π a = π' b       *)
+    (*  [], Δ |- a = (rev π @ π') b  *)
+    (* ----------------------------- *)
+    (*     [], Δ |- π a = π' b       *)
     let a' = atom a in
     let b' = atom' (permute (reverse pi) b) in
-    solve_eq env assms a' b'
+    solve_eq env a' b'
   | T_Atom _, _ -> false
   | T_Var {perm= []; symb= x}, T_Var {perm= pi; symb= x'} when x = x' ->
-    (*  Γ, Δ |- π idempotent on x  *)
+    (*  [], Δ |- π idempotent on x  *)
     (* --------------------------- *)
-    (*       Γ, Δ |- x = π x       *)
-    permutation_idempotent env assms pi x
+    (*       [], Δ |- x = π x       *)
+    permutation_idempotent env pi x
   | T_Var {perm= pi; symb= x}, T_Var {symb= x'; _} when x = x' ->
-    (*  Γ, Δ |- x = (rev π @ π') x  *)
+    (*  [], Δ |- x = (rev π @ π') x  *)
     (* ---------------------------- *)
-    (*     Γ, Δ |- π x = π' x       *)
-    solve_eq env assms (var x) (permute_term (reverse pi) e2)
+    (*     [], Δ |- π x = π' x       *)
+    solve_eq env (var x) (permute_term (reverse pi) e2)
   | T_Var _, _ -> false
   | T_Lam (a1, t1), T_Lam (a2, t2) ->
-    (*    Γ, Δ |- a1 # (a2.e2)   *)
-    (*  Γ, Δ |- e1 = [a1 a2] e2  *)
+    (*    [], Δ |- a1 # (a2.e2)   *)
+    (*  [], Δ |- e1 = [a1 a2] e2  *)
     (* ------------------------- *)
-    (*   Γ, Δ |- a1.e1 = a2.e2   *)
-    solve env assms $ fresh a1 e2 && solve_eq env assms t1 $ permute_term [(a1, a2)] t2
+    (*   [], Δ |- a1.e1 = a2.e2   *)
+    solve env [] $ fresh a1 e2 && solve_eq env t1 $ permute_term [(a1, a2)] t2
   | T_Lam _, _ -> false
   | T_App (t1, t2), T_App (t1', t2') ->
-    (*      Γ, Δ |- e1 = e2      *)
-    (*     Γ, Δ |- e1' = e2'     *)
+    (*      [], Δ |- e1 = e2      *)
+    (*     [], Δ |- e1' = e2'     *)
     (* ------------------------- *)
-    (*  Γ, Δ |- e1 e1' = e2 e2'  *)
-    solve_eq env assms t1 t1' && solve_eq env assms t2 t2'
+    (*  [], Δ |- e1 e1' = e2 e2'  *)
+    solve_eq env t1 t1' && solve_eq env t2 t2'
   | T_App _, _ -> false
   | T_Fun f1, T_Fun f2 ->
     (* --------------- *)
-    (*  Γ, Δ |- f = f  *)
+    (*  [], Δ |- f = f  *)
     f1 = f2
   | T_Fun _, _ -> false
 
-and permutation_idempotent env assms pi x =
-  (*  ∀ a ∈ π. (Γ, Δ |- a = π a  ∨  Γ, Δ |- a # x)  *)
+and permutation_idempotent env pi x =
+  (*  ∀ a ∈ π. ([], Δ |- a = π a  ∨  [], Δ |- a # x)  *)
   (* ---------------------------------------------- *)
-  (*            Γ, Δ |- π idempotent on x           *)
+  (*            [], Δ |- π idempotent on x           *)
   let test ({perm; symb= a} as alpha) =
-    solve_eq env assms (atom' alpha) (atom' (permute pi alpha))
-    || solve_fresh env assms a (var' {perm= reverse perm; symb= x})
+    solve_eq env (atom' alpha) (atom' (permute pi alpha)) || solve_fresh env a (var' {perm= reverse perm; symb= x})
   in
   List.for_all test (free_vars_of pi)
 
-and solve_fresh env assms a e =
-  let solve_fresh_swap swap t = solve_swap_cases env a swap (const assms) (flip fresh t) in
+and solve_fresh env a e =
+  let solve_fresh_swap swap t = solve_swap_cases env a swap (const []) (flip fresh t) in
   match e with
   | T_Atom {perm; symb= b} -> (
     match outer_swap perm with
     | None ->
       (*  (a =/= b) ∈ Δ  *)
       (* --------------- *)
-      (*  Γ, Δ |- a # b  *)
+      (*  [], Δ |- a # b  *)
       SolverEnv.is_neq env a b
     | Some (swap, pi) ->
-      (*  (a =/= a1) :: (a =/= a2) :: Γ, Δ |-  a # π b  *)
-      (*    (a = a1) :: (a =/= a2) :: Γ, Δ |- a2 # π b  *)
-      (*                  (a = a2) :: Γ, Δ |- a1 # π b  *)
-      (* ---------------------------------------------------- *)
-      (*              Γ, Δ |- a # [a1 a2]π b            *)
+      (*  [ a =/= a1, a =/= a2 ], Δ |-  a # π b  *)
+      (*    [ a = a1, a =/= a2 ], Δ |- a2 # π b  *)
+      (*               [ a = a2 ], Δ |- a1 # π b  *)
+      (* --------------------------------------- *)
+      (*           [], Δ |- a # [a1 a2]π b       *)
       solve_fresh_swap swap (atom' {perm= pi; symb= b}) )
   | T_Var {perm; symb= x} -> (
     match outer_swap perm with
     | None ->
       SolverEnv.is_fresh env a x || SolverEnv.is_symbol env x
-      (*   (a # x) ∈ Δ        (symbol x) ∈ Δ  *)
-      (* ---------------    ----------------- *)
-      (*  Γ, Δ |- a # x       Γ, Δ |- a # x   *)
+      (*   (a # x) ∈ Δ          (symbol x) ∈ Δ  *)
+      (* ----------------    ------------------ *)
+      (*  [], Δ |- a # x       [], Δ |- a # x   *)
     | Some (swap, pi) ->
-      (*  (a # a1) :: (a # a2) :: Γ, Δ |-  a # π x  *)
-      (*  (a = a1) :: (a # a2) :: Γ, Δ |- a2 # π x  *)
-      (*              (a = a2) :: Γ, Δ |- a1 # π x  *)
-      (* ------------------------------------- ---- *)
-      (*            Γ, Δ |- a # [a1 a2]π x          *)
+      (*  [ a # a1, a # a2 ], Δ |-  a # π x  *)
+      (*  [ a = a1, a # a2 ], Δ |- a2 # π x  *)
+      (*          [ a = a2 ], Δ |- a1 # π x  *)
+      (* ----------------------------------- *)
+      (*         [], Δ |- a # [a1 a2]π x     *)
       solve_fresh_swap swap $ var' {perm= pi; symb= x} )
   | T_Lam (alpha, t) ->
-    (*  (a # b) :: Γ, Δ |- a # e  *)
-    (* -------------------------- *)
-    (*      Γ, Δ |- a # b.e       *)
-    solve env ((a =/=: alpha) :: assms) $ a #: t
+    (*  [ a # b ], Δ |- a # e  *)
+    (* ----------------------- *)
+    (*    [], Δ |- a # b.e     *)
+    solve env [a =/=: alpha] $ a #: t
   | T_App (t1, t2) ->
-    (*    Γ, Δ |- a # e1    *)
-    (*    Γ, Δ |- a # e2    *)
+    (*   [], Δ |- a # e1    *)
+    (*   [], Δ |- a # e2    *)
     (* -------------------- *)
-    (*   Γ, Δ |- a # e1 e2  *)
-    solve_fresh env assms a t1 && solve_fresh env assms a t2
+    (*  [], Δ |- a # e1 e2  *)
+    solve_fresh env a t1 && solve_fresh env a t2
   | T_Fun _ ->
     (* absurd *)
     true
 
-and solve_shape env assms s1 s2 =
+and solve_shape env s1 s2 =
   match (s1, s2) with
   | S_Var x1, S_Var x2 ->
     (*    (x1 ~ x2) ∈ Δ   *)
     (* ------------------ *)
-    (*  Γ, Δ |- x1 ~ x2   *)
+    (*  [], Δ |- x1 ~ x2  *)
     SolverEnv.are_same_shape env x1 x2
   | S_Var x, s | s, S_Var x -> (
     match SolverEnv.get_shape env x with
     | None -> false
-    (*    (x ~ s') ∈ Δ   *)
-    (*   Γ, Δ |- s' ~ s  *)
-    (* ----------------- *)
-    (*   Γ, Δ |- x ~ s   *)
-    | Some sx -> solve_shape env assms sx s )
+    (*   (x ~ z) ∈ Δ    *)
+    (*  [], Δ |- z ~ s  *)
+    (* ---------------- *)
+    (*  [], Δ |- x ~ z  *)
+    | Some sx -> solve_shape env sx s )
   | S_Atom, S_Atom ->
     (* ------------------- *)
-    (*   Γ, Δ |- a1 ~ a2   *)
+    (*   [], Δ |- a1 ~ a2   *)
     true
   | S_Atom, _ -> false
   | S_Lam s1, S_Lam s2 ->
-    (*    Γ, Δ |- e1 ~ e2    *)
-    (* --------------------- *)
-    (*  Γ, Δ |- _.e1 ~ _.e2  *)
-    solve_shape env assms s1 s2
+    (*    [], Δ |- e1 ~ e2    *)
+    (* ---------------------- *)
+    (*  [], Δ |- _.e1 ~ _.e2  *)
+    solve_shape env s1 s2
   | S_Lam _, _ -> false
   | S_App (s1, s1'), S_App (s2, s2') ->
-    (*      Γ, Δ |- e1 ~ e2      *)
-    (*     Γ, Δ |- e1' ~ e2'     *)
-    (* ------------------------- *)
-    (*  Γ, Δ |- e1 e1' ~ e2 e2'  *)
-    solve_shape env assms s1 s2 && solve_shape env assms s1' s2'
+    (*      [], Δ |- e1 ~ e2      *)
+    (*     [], Δ |- e1' ~ e2'     *)
+    (* -------------------------- *)
+    (*  [], Δ |- e1 e1' ~ e2 e2'  *)
+    solve_shape env s1 s2 && solve_shape env s1' s2'
   | S_App _, _ -> false
   | S_Fun f1, S_Fun f2 ->
-    (* --------------- *)
-    (*  Γ, Δ |- f ~ f  *)
+    (* ---------------- *)
+    (*  [], Δ |- f ~ f  *)
     f1 = f2
   | S_Fun _, _ -> false
 
-and solve_subshape env assms t1 = function
+and solve_subshape env t1 = function
   | S_Var x ->
-    (*     (t2 < x) ∈ Δ             (t2 < x) ∈ Δ    *)
-    (*   Γ, Δ |- t1 ~ t2          Γ, Δ |- t1 < t2   *)
+    (*    (t2 < x) ∈ Δ              (t2 < x) ∈ Δ    *)
+    (*  [], Δ |- t1 ~ t2          [], Δ |- t1 < t2  *)
     (* -------------------      ------------------- *)
-    (*   Γ, Δ |- t1 < x           Γ, Δ |- t1 < x    *)
-    List.exists (solve_shape_or_subshape env assms t1) (SolverEnv.get_subshapes env x)
+    (*  [], Δ |- t1 < x           [], Δ |- t1 < x   *)
+    List.exists (solve_shape_or_subshape env t1) (SolverEnv.get_subshapes env x)
   | S_Lam t2 ->
-    (*   Γ, Δ |- t1 ~ t2          Γ, Δ |- t1 < t2   *)
-    (* -------------------      ------------------- *)
-    (*  Γ, Δ |- t1 < _.t2        Γ, Δ |- t1 < _.t2  *)
-    solve_shape_or_subshape env assms t1 t2
+    (*   [], Δ |- t1 ~ t2          [], Δ |- t1 < t2   *)
+    (* --------------------      -------------------- *)
+    (*  [], Δ |- t1 < _.t2        [], Δ |- t1 < _.t2  *)
+    solve_shape_or_subshape env t1 t2
   | S_App (t2, t2') ->
-    (*   Γ, Δ |- t1 ~ t2         Γ, Δ |- t1 < t2          Γ, Δ |- t1 ~ t2'         Γ, Δ |- t1 < t2'    *)
-    (* ---------------------   ---------------------   ----------------------   ---------------------- *)
-    (*  Γ, Δ |- t1 < t2 t2'     Γ, Δ |- t1 < t2 t2'      Γ, Δ |- t1 < t2 t2'      Γ, Δ |- t1 < t2 t2'  *)
-    solve_shape_or_subshape env assms t1 t2 || solve_shape_or_subshape env assms t1 t2'
+    (*   [], Δ |- t1 ~ t2         [], Δ |- t1 < t2          [], Δ |- t1 ~ t2'         [], Δ |- t1 < t2'    *)
+    (* ----------------------   ----------------------   -----------------------   ----------------------- *)
+    (*  [], Δ |- t1 < t2 t2'     [], Δ |- t1 < t2 t2'      [], Δ |- t1 < t2 t2'      [], Δ |- t1 < t2 t2'  *)
+    solve_shape_or_subshape env t1 t2 || solve_shape_or_subshape env t1 t2'
   | S_Atom | S_Fun _ ->
     (* trivial *)
     false
 
-and solve_shape_or_subshape env assms t1 t2 = solve_shape env assms t1 t2 || solve_subshape env assms t1 t2
+and solve_shape_or_subshape env t1 t2 = solve_shape env t1 t2 || solve_subshape env t1 t2
 
 and solve_symbol env = function
   | S_Fun _ ->
-    (* ------------------ *)
-    (*  Γ, Δ |- symbol f  *)
+    (* ------------------- *)
+    (*  [], Δ |- symbol f  *)
     true
   | S_Var x ->
     (*    (symbol x) ∈ Δ   *)
-    (* -------------------- *)
-    (*   Γ, Δ |- symbol x  *)
+    (* ------------------- *)
+    (*  [], Δ |- symbol x  *)
     SolverEnv.is_symbol env x
   | S_Atom | S_Lam _ | S_App _ ->
     (* trivial *)
@@ -273,11 +272,11 @@ and solve_assm_fresh env assms goal a = function
 
 and solve_assm_eq env assms goal t1 t2 =
   match (t1, t2) with
-  | T_Var {perm= []; symb= x}, T_Var {perm= pi; symb= x'} when x = x' && permutation_idempotent env [] pi x ->
-    (*   |- π idempotent on x   *)
-    (*         Γ, Δ |- c        *)
-    (* ------------------------ *)
-    (*  (x = π x) :: Γ, Δ |- c  *)
+  | T_Var {perm= []; symb= x}, T_Var {perm= pi; symb= x'} when x = x' && permutation_idempotent env pi x ->
+    (*  [], Δ |- π idempotent on x  *)
+    (*           Γ, Δ |- c          *)
+    (* ---------------------------- *)
+    (*    (x = π x) :: Γ, Δ |- c    *)
     solve env assms goal
   | T_Var {perm= []; symb= x}, T_Var {perm= pi; symb= x'} when x = x' ->
     (*  (π idempotent on x) :: Γ, Δ |- c  *)
